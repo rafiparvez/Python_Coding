1e7c3f8dccce83be14481c185617089cfc3f6bee
Parvez Rafi <rafiparvez@gmail.com>
2018-03-18 03:05:04 -0500

    new approach permutations

--- EPI/15_Recursion/15.03_Generate_Permutations/15.03_var_1_Generate_Permutations.py
+++ EPI/15_Recursion/15.03_Generate_Permutations/15.03_var_1_Generate_Permutations.py
@@ -16,3 +16,32 @@ arr=[1,2,2]
 print(permutation(arr))
 
 
+#### approach 2
+
+from collections import Counter
+from copy import deepcopy
+
+def permute_ii(arr):
+    result = []
+    hmap = Counter(arr)
+
+    def recurse(arr, idx):
+        if idx == len(arr):
+            result.append(deepcopy(arr))
+            return
+
+        for key in hmap.keys():
+            if hmap[key] == 0:
+                continue
+            hmap[key] -= 1
+            arr[idx], key = key, arr[idx]
+            recurse(arr, idx+1)
+            arr[idx], key = key, arr[idx] # backtrack
+            hmap[key] += 1 # backtrack
+
+    recurse(arr, 0)
+    return result
+
+arr = [2,3,2]
+print(permute_ii(arr))
+
